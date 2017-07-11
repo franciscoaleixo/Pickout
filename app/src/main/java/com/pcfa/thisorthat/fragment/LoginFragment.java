97aa9613db026b18f9461d7d66f807e0f6d98d2f
franciscoaleixo <francisco.aleixo16@gmail.com>
2017-07-11 01:33:38 +0100

    Minor arrangements and preparing button listeners for Login

--- app/src/main/java/com/pcfa/thisorthat/fragment/LoginFragment.java
+++ app/src/main/java/com/pcfa/thisorthat/fragment/LoginFragment.java
@@ -8,6 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
 import com.pcfa.thisorthat.R;
 import com.pcfa.thisorthat.activity.MainActivity;
@@ -24,6 +25,8 @@ public class LoginFragment extends Fragment {
     public static final String TAG = LoginFragment.class.getSimpleName();
 
     @BindView(R.id.loginBtn) Button loginButton;
+    @BindView(R.id.reg_btn) Button regButton;
+    @BindView(R.id.facebook_login) LinearLayout fbLogin;
 
     public LoginFragment() {
         // Required empty public constructor
@@ -40,19 +43,41 @@ public class LoginFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_login, container, false);
         ButterKnife.bind(this, view);
 
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(LoginFragment.this.getContext(), MainActivity.class);
-                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
-                startActivity(intent);
-                LoginFragment.this.getActivity().finish();
-            }
-        });
+        setUpClickListeners();
+
 
         return view;
     }
 
+    private void setUpClickListeners() {
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startMainActivity();
+            }
+        });
+        fbLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Login with facebook
+            }
+        });
+        regButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Register
+            }
+        });
+
+    }
+
+    private void startMainActivity() {
+        Intent intent = new Intent(LoginFragment.this.getActivity(), MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+        LoginFragment.this.getActivity().finish();
+    }
+    
 
 
 }
